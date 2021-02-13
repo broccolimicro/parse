ab778c9f0b7b7284114a254e1f56bd131c27297b
Ned Bingham <talon4196@gmail.com>
2021-02-13 16:11:49 -0500

    changing build system to tup

--- parse/parse.h
+++ parse/parse.h
@@ -5,13 +5,8 @@
  *      Author: nbingham
  */
 
-#ifndef parse_parse_h
-#define parse_parse_h
-
 #include "token.h"
 #include "segment.h"
 #include "tokenizer.h"
 #include "configuration.h"
 #include "syntax.h"
-
-#endif
