5bc236b55ba62a5754e83c9a33d46b7a7bba9518
Ned Bingham <edward.bingham@broccolimicro.io>
2026-06-28 12:32:02 -0400

    adding factory

---
+++ parse/factory.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "parse.h"
+
+namespace parse {
+
+struct factory {
+	typedef void (*Register)(tokenizer&);
+	typedef void (*Expect)(tokenizer&);
+	typedef parse::syntax *(*Produce)(tokenizer&, void*);
+
+	const Register register_syntax;
+	const Expect expect;
+	const Produce produce;
+};
+
+struct registry {
+	virtual const factory *getParser(std::string lang) const = 0;
+	virtual std::vector<std::string> getParserIndex() const = 0;
+};
+
+}
